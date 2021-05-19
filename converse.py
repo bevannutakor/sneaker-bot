7814593c899d1bd12769f7b56e73e885a44e4662
bevannutakor <bevannutakor@icloud.com>
2021-05-18 21:27:15 -0700

    pain

--- converse.py
+++ converse.py
@@ -22,40 +22,9 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36"
 }
 
-def search_for_all_items():
-    items_url = base_url + '/men/sneakers/all-sneakers/'
-    browser.get(items_url)
-    wait = WebDriverWait(browser, 10)
-    while True:
-        try:
-            browser.execute_script("return arguments[0].scrollIntoView(true);", wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='m-show-more']"))))
-
-            browser.execute_script("arguments[0].click();", wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='m-show-more']"))))
-
-            print("Button clicked")
-        except TimeoutException:
-            print("All show more buttons have been clicked")
-            break
-    session = HTMLSession()
-    session.headers.update(headers)
-    cookies = browser.get_cookies() 
-
-    for cookie in cookies:
-        c = {cookie['name']: cookie['value']}
-        session.cookies.update(c)
-
-    response = session.get(browser.current_url)
-    list_of_all_items = response.html.find('.products-grid', first=True).find('.product-name')
-    items = []
-
-    for i in list_of_all_items:
-        items.append(i.text)
-    
-    browser.quit()
-    return items
-
 def search_specific_item():
-    pass
+    wanted_item = input("type in the item wanted")
+    product_url = base_url + wanted_item
     
 
 def check_item_availability():
