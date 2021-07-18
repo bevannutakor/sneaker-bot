6e64fee1803e5f78b126a0b4db965e758c5460d7
bevannutakor <bevannutakor@icloud.com>
2021-07-18 16:05:01 -0700

    changes

--- converse.py
+++ converse.py
@@ -1,3 +1,5 @@
+import bs4
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,54 +10,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.webdriver.common.keys import Keys
 
-#from selenium.webdriver.chrome.options import Options
-
-#from requests_html import HTMLSession, AsyncHTMLSession
-import requests
-
-import time
-import os
-
 base_url = 'https://www.converse.ca/'
-
 driver = webdriver.Chrome('/Users/user/Desktop/chromedriver')
 
-proxies = []
-
-'''headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36"
-}'''
-
-def generate_item_url():
-    wanted_item_keywords = input("Tell us the item you want to purchase(Include spaces for each word and Be detailed): ")
-    model_name = wanted_item_keywords.replace(" ", "-")
-    full_url = base_url  + model_name + '.html'
-
-    return full_url
-
-def add_item_to_cart():
-    item_url = generate_item_url()
-    driver.get(item_url)
-
-    #select size and quantity code block
-    size_input = input("indicate the size the size shoe: ")
-    select_size_button = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, "//*[@id='attribute136']")))
-
-    select_size_button.click()
-
-    quantity = input("indicate the amount of the item: ")
-
-
-    checkout_url = driver.current_url
-
-    driver.quit()
-
-    return checkout_url
-    
-
-def complete_purchase():
-    url = add_item_to_cart()
-    
+def generate_url():
     pass
 
-print(add_item_to_cart())
+def variant():
+    pass
+
+def checkout():
+    pass
